9677e573e79ef2f5b902c7052c4e716a72259ff4
Lexh19 <iqbaalhzd@gmail.com>
2024-10-19 02:08:17 +0700

    fix color toggle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
                 <a class="navbar-brand" href="#page-top">
                     <img src="assets/img/logo/logo-sawahan.png"  height="70%" width="90%">
                 </a>
-                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-black rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
                 </button>
